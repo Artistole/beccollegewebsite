a026be135ec7932e64bf4676eae4a41a81567fde
Padmaja-Vinta <padmajareddyvinta@gmail.com>
2023-03-08 23:18:58 +0530

    placement data service

--- src/config/services.js
+++ src/config/services.js
@@ -4,3 +4,7 @@ import { AxiosInstance } from './config';
 export const getLandingPageData = () => {
     return AxiosInstance.get('/getAllLandingPageData');
 }
+
+export const getPlacementData = () => {
+    return AxiosInstance.get('/getPlacementsData');
+}
